de1fda073ec95fc80218d19db12a5126256f36b7
Apollo Tang <tang.apollo@gmail.com>
2014-11-12 12:34:35 -0500

    play with jasmine-jquery's fixture

--- 02-jasmine-jquery/spec/01-fixtures.js
+++ 02-jasmine-jquery/spec/01-fixtures.js
@@ -1,49 +1,62 @@
 describe("checking out fixture:", function(){
-    // xxx
-        // var ajaxData = 'some ajax data'
-        // var fixtureUrl = 'some_url'
-        // var anotherFixtureUrl = 'another_url'
-        // var fixturesContainer = function () {
-        //     return $('#' + jasmine.getFixtures().containerId)
-        // }
-        // var appendFixturesContainerToDom = function () {
-        //     $('body').append('<div id="' + jasmine.getFixtures().containerId + '">old content</div>')
-        // }
-        //
-        // beforeEach(function () {
-        //     jasmine.getFixtures().clearCache()
-        //     spyOn(jasmine.Fixtures.prototype, 'loadFixtureIntoCache_').and.callFake(function (relativeUrl){
-        //         this.fixturesCache_[relativeUrl] = ajaxData
-        //     })
-        // })
-        //
-        // describe("default initial config values", function () {
-        //     it("should set 'jasmine-fixtures' as the default container id", function () {
-        //         expect(jasmine.getFixtures().containerId).toEqual('jasmine-fixtures');
-        //         console.log(jasmine.getFixtures())
-        //     })
-        //
-        //     it("should set 'spec/javascripts/fixtures' as the default fixtures path", function () {
-        //         expect(jasmine.getFixtures().fixturesPath).toEqual('spec/javascripts/fixtures')
-        //     })
-        // })
 
     it("offers 3 crucial functions", function(){
+        expect(loadFixtures).toBeDefined();
         expect(readFixtures).toBeDefined();
         expect(setFixtures).toBeDefined();
-        expect(loadFixtures).toBeDefined();
     });
 
     it("can load fixtures from a file, and append it to dom", function(){
         loadFixtures('myFragment.html');
-        //default location: spec/javascripts/fixtures/myFragment.html
+        //default location for fragment files:
+        //  "spec/javascripts/fixtures/myFragment.html"
         expect($('.fragment')).toExist();
-        // debugger;  <-- if you pause here you will see it in the dom!
+        // debugger; // <-- if you pause here you will see fragment in the dom!
+        //           //     The fragment is appended as the inner html
+        //           //     of <div id="jasmine-fixtures"></div>
     });
 
     it("can read fixtures w/o appending to DOM", function(){
+        // the different between loadFixtures and readFixtures
+        // is taht 'loadFixtures' append the fragment in the dom
+        // while 'readFixtures' return fragment as a string without
+        // inserting the fragment into DOM.
         var fixture = readFixtures('myFragment.html');
         expect( fixture ).toContain('li');
         expect( $(fixture).find('li') ).toHaveText('Add some data');
     });
+
+    it("can also receive the fixture from a parameter instead of a file", function(){
+        setFixtures('<div class="sandbox">this is a sand box</div>');
+        // unlike loadFixtures, you pass in string instead of
+        // filename of the fragment. The fragment is then appended
+        // to the DOM like loadFixtures.
+        // In addition, in stead of string you can also pass in
+        // jQuery wapper set
+        expect( $('.sandbox')).toExist();
+        // debugger; // <-- if you pause here you will see fragment in the dom!
+    });
+
+    // -- Sandbox function --//
+
+    it("offer a sandbox function that wrap in jQuery", function(){
+        expect(sandbox).toBeDefined;
+        var $sb = sandbox();
+        expect($sb instanceof jQuery).toBeTruthy()
+    });
+
+    it("can specify class for the sand box", function(){
+        expect(sandbox).toBeDefined;
+        var $sb = sandbox({'class':'some-class'});
+        expect($sb).toHaveClass('some-class');
+    });
+
+    it("can pass sandbox to setFixtures(), which will insert it in DOM", function(){
+        expect(sandbox).toBeDefined;
+        var $sb = sandbox({'class':'some-class'});
+        setFixtures($sb);
+        expect($('.some-class')).toExist();
+        expect($('#sandbox')).toHaveClass('some-class');
+    });
+
 })
