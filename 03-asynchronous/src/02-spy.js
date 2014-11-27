8b523aeaa93dc5c96cfe0336558c4ac16d25a339
Apollo Tang <tang.apollo@gmail.com>
2014-11-27 13:02:59 -0500

    move synchronous spec in to a decicated asynchronous study folder

---
+++ 03-asynchronous/src/02-spy.js
@@ -0,0 +1,22 @@
+
+myObj = {
+    someMethod: function () {
+        console.log('someMethod called');
+        // $.get('someFile.html', function(data){
+        //     return data;
+        // })
+        return 'hello there'
+    }
+}
+
+
+$.fn.asyncCall = function() {
+    return this.each(function(){
+        var el = $(this);
+
+        $.get('someFile.html', function(data){
+            console.log('data: ', data)
+            el.html(data);
+        })
+    })
+}
